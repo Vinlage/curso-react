9fea4013266c81740d13d028fe5a1cece12c284f
ViniciusLage <vinicius_clage@yahoo.com.br>
2019-01-01 12:49:46 -0200

    React Firebase Tutorial

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
 class App extends Component {
+  state = 
+  {
+    ninjas: 
+    [
+      {name: 'Vinicius', age: 28, belt: 'black', id: 1},
+      {name: 'Jessica', age: 30, belt: 'green', id: 2},
+      {name: 'Victoria', age: 5, belt: 'pink', id: 3}
+    ]
+  }
+  addNinja = (ninja) =>
+  {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja]
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+  deleteNinja = (id) =>
+  {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id
+    });
+    this.setState({
+      ninjas: ninjas
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>My first React app!</h1>
+        <p>Welcome</p>
+        <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas}/>
+        <AddNinja addNinja={this.addNinja}/>
       </div>
     );
   }
